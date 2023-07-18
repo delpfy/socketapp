dd128529d2cca9c4426783f180957686b8b1a66e
delpfy <cubaru00@gmail.com>
2023-07-18 20:19:57 +0300

    forgot to change axios url

--- src/redux/axios.ts
+++ src/redux/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000"
-  //baseURL: "https://socket-express.vercel.app",
+  //baseURL: "http://localhost:4000"
+  baseURL: "https://socket-express.vercel.app",
   //baseURL: process.env.REACT_APP_API_URL,
 });
 
