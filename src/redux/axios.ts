1d767b04c02ed9425d65eb32f31234c38590b41b
delpfy <cubaru00@gmail.com>
2023-08-31 16:12:46 +0300

    fix

--- src/redux/axios.ts
+++ src/redux/axios.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
- /*  baseURL: "http://localhost:4000" */
-  /* baseURL: "https://socket-express.vercel.app", */
-  baseURL: "https://www.sidebyside-tech.com/",
+  /* baseURL: "http://localhost:4000" */
+  baseURL: "https://socket-express.vercel.app",
+  /* baseURL: "https://www.sidebyside-tech.com/", */
  
  /*  baseURL: process.env.REACT_APP_API_URL, */
 });
