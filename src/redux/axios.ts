4d806d0a3295f9c9c7f4189132f72741312f6876
delpfy <cubaru00@gmail.com>
2025-01-29 22:21:28 +0200

    fix

--- src/redux/axios.ts
+++ src/redux/axios.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const instance = axios.create({
   /* baseURL: "https://www.sidebyside-tech.cm" */
-  /* baseURL: "https://socket-express-bssu.onrender.com" */
+  baseURL: "https://socket-express-bssu.onrender.com"
   
-  baseURL: "http://localhost:4000"
+  /* baseURL: "http://localhost:4000" */
   /* baseURL: "https://socket-express.vercel.app", */
   /* baseURL: "https://socket-express-bssu.onrender.com/" */,
   /*  baseURL: process.env.REACT_APP_API_URL, */
